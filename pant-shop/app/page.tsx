cd4226faa5ca6987cc26daf603acf92080c2672d
vincentfarka <edlirabozgo@gmail.com>
2026-09-13 23:27:00 +0000

    added some animation

--- pant-shop/app/page.tsx
+++ pant-shop/app/page.tsx
@@ -1,40 +1,64 @@
+"use client";
+
 import MotionDiv from "@/components/div";
 import Reveal from "@/components/reveal";
 import { ArrowBigDown } from "lucide-react";
-import { Button } from "@/components/ui/button";
-import { ArrowRight, Camera } from "lucide-react";
-import Image from "next/image";
-import Link from "next/link";
-import { spring } from "motion";
+import {
+  useAnimationControls,
+  useMotionValueEvent,
+  useScroll,
+  useTransform,
+} from "motion/react";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const { scrollY } = useScroll();
+  useMotionValueEvent(scrollY, "change", (latest) => {
+    if (latest >= 100) {
+      blankControls.start("end");
+    } else {
+      blankControls.start("start");
+    }
+  });
+  const opacity = useTransform(scrollY, [0, 200], [1, 0]);
+  const blankControls = useAnimationControls();
   const revealDelay = 0.1;
   return (
     <>
-    <div className="w-screen h-screen flex justify-center">
-      <Reveal revealDelay={revealDelay} className="fixed top-1/2 left-1/2 -translate-x-1/2">
-        <h1 className="text-5xl whitespace-nowrap overflow-hidden text-primary flex gap-2">
-          Blank{" "}
-          <div className="flex gap-2">
-            [<div />]
-          </div>
-        </h1>
-      </Reveal>
-      <MotionDiv
-        className="absolute top-[70%] text-2xl text-primary font-light flex gap-2 items-center"
-        initial={{ y: 10 }}
-        animate={{ y: 0 }}
-        transition={{
-          type: "tween",
-          duration: 0.5,
-          repeat: Infinity,
-          repeatType: "reverse",
-        }}
-      >
-        Scroll Down <ArrowBigDown className="" />
-      </MotionDiv>
-    </div>
-    <div className="min-h-screen min-w-screen flex items-center justify-center text-7xl font-black"><h1>hi</h1></div>
+      <div className="w-screen min-h-screen flex justify-center items-center relative">
+        <Reveal revealDelay={revealDelay} className="fixed">
+          <h1 className="text-5xl whitespace-nowrap overflow-hidden text-primary flex gap-2 px-2 relative">
+            <MotionDiv style={{ opacity }}>bblankk</MotionDiv>
+            <MotionDiv
+              className="flex gap-2 absolute"
+              variants={{
+                start: { left: "100%", x: "0%" },
+                end: { left: "50%", x: "-50%" },
+              }}
+              initial="start"
+              animate={blankControls}
+              transition={{ type: "tween", ease:"easeOut", }}
+            >
+              [<div />]
+            </MotionDiv>
+          </h1>
+        </Reveal>
+        <MotionDiv
+          style={{ opacity }}
+          className="absolute top-[70%] text-2xl text-primary font-light flex gap-2 items-center"
+          initial={{ y: 10 }}
+          animate={{ y: 0 }}
+          transition={{
+            type: "tween",
+            duration: 0.5,
+            repeat: Infinity,
+            repeatType: "reverse",
+          }}
+        >
+          Scroll Down <ArrowBigDown className="" />
+        </MotionDiv>
+      </div>
+      <div className="w-full min-h-[200px]"></div>
     </>
   );
 }
